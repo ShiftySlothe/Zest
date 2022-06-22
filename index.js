2d08728a554e7d15d94419ce8af17488ad955fd8
ShiftySlothe <forson.charlesj@gmail.com>
2022-06-22 13:36:20 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = async function (name, params = {}) {
+  const file = path.join(process.cwd(), "spec", "tests", `${name}.test.js`);
+  const tests = require(file);
+  for (const test in tests) {
+    try {
+      await tests[test](params);
+    } catch (e) {
+      console.log(`\n* it['${test}'] in '${name}' failed:\n`);
+      console.log(e.message);
+      console.log();
+      process.exit(0);
+    }
+  }
+};
